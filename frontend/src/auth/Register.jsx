0aea6b2321d5a21120e5ad303c5086b0462879b4
shuvam773 <shuvam.sk25@gmail.com>
2025-10-03 18:50:43 +0530

    register page updated

--- frontend/src/auth/Register.jsx
+++ frontend/src/auth/Register.jsx
@@ -165,25 +165,6 @@ const Register = () => {
             </div>
 
             <div>
-              <label
-                htmlFor="role"
-                className="block text-sm font-medium text-gray-700"
-              >
-                Role
-              </label>
-              <select
-                onChange={handleChange}
-                name="role"
-                required
-                className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
-                value={signupInfo.role || 'student'}
-              >
-                <option value="student">Student</option>
-                <option value="teacher">Teacher</option>
-              </select>
-            </div>
-
-            <div>
               <button
                 type="submit"
                 disabled={isLoading}
